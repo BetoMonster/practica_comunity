c8a9be5c0cbecdc74f8cf9d3ac1d23643d52418f
Diana Carhuamanta <dianacarhuamanta03@gmail.com>
2021-04-10 23:51:24 -0500

    imprimiendo Cards Pokemons

--- js/main.js
+++ js/main.js
@@ -1,32 +1,32 @@
 
-const palindromo = (palabra) => {
-  // console.log(palabra);
-  let wordModified = palabra.split("").reverse().join("").replace(/\s/g, "").toLowerCase();
-  console.log(wordModified);
-  let wordOrigin = palabra.toLowerCase().replace(/\s/g, "");
-  console.log(wordOrigin);
-  if (wordModified === wordOrigin) {
+// const palindromo = (palabra) => {
+//   // console.log(palabra);
+//   let wordModified = palabra.split("").reverse().join("").replace(/\s/g, "").toLowerCase();
+//   console.log(wordModified);
+//   let wordOrigin = palabra.toLowerCase().replace(/\s/g, "");
+//   console.log(wordOrigin);
+//   if (wordModified === wordOrigin) {
 
-    printPalindrome(`La palabra ${palabra} es palindromo`);
+//     printPalindrome(`La palabra ${palabra} es palindromo`);
 
-  }
-  else {
-    printPalindrome(`La palabra ${palabra} NO es palindromo`);
-  }
+//   }
+//   else {
+//     printPalindrome(`La palabra ${palabra} NO es palindromo`);
+//   }
 
-};
+// };
 
-palindromo("Anita lava la tina");
+// palindromo("Anita lava la tina");
 
-function printPalindrome(phrase) {
+// function printPalindrome(phrase) {
 
-  const container = document.getElementById('palindrome')
-  const paragraph = document.createElement('p')
-  const text = document.createTextNode(phrase)
+//   const container = document.getElementById('palindrome')
+//   const paragraph = document.createElement('p')
+//   const text = document.createTextNode(phrase)
 
-  paragraph.appendChild(text)
-  container.appendChild(paragraph)
-}
+//   paragraph.appendChild(text)
+//   container.appendChild(paragraph)
+// }
 
 //https://pokeapi.co/api/v2/pokemon
 
@@ -39,8 +39,7 @@ function getPokemon() {
             let pokemonsArr = JSON.parse(this.responseText).results
 
             pokemonsArr.forEach(pokemon => {
-                 getPokemonUrl(pokemon.url)//console.log(pokemon.url)
-                
+                 printPokemon(getPokemonUrl(pokemon.url))
             });
 
 
@@ -55,6 +54,7 @@ function getPokemon() {
 getPokemon()
 
 function getPokemonUrl (url) {
+  // console.log(url)
   let pokemonInfo ={} 
   let xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
@@ -75,11 +75,38 @@ function getPokemonUrl (url) {
         else if (key === "sprites") {
           pokemonInfo = {...pokemonInfo, image:responseData[key]["other"]["dream_world"].front_default}
         }
-      }console.log (pokemonInfo)
+      }
     } 
 }; 
-xhttp.open("GET", url, true);
+// console.log(pokemonInfo)   
+xhttp.open("GET", url, false);
 xhttp.send();
- return pokemonInfo
+return pokemonInfo
 }
+
+function printPokemon (infoData) {
+    let cardContainer = document.getElementById("container")
+    let cardElement = document.createElement("div")
+    let cardH1 = document.createElement("h1")
+    let cardImage = document.createElement("img")
+    let cardLista = document.createElement("ul")
+
+    let txtH1 = document.createTextNode(infoData.name)
+    cardImage.setAttribute("src",infoData.image)
+    infoData.stats.forEach(stat=>{
+      let cardLi = document.createElement("li")
+      let cardText = document.createTextNode(`${stat.stat.name}:${stat.base_stat}`)
+      cardLi.appendChild(cardText)
+      cardLista.appendChild(cardLi)
+    })
+    cardElement.classList.add("card")
+
+    cardH1.appendChild(txtH1)
+    cardElement.appendChild(cardH1)
+    cardElement.appendChild(cardImage)
+    cardElement.appendChild(cardLista)
+    cardContainer.appendChild(cardElement)
+  console.log(infoData)
+}
+// classList.add("card")
 
